ceadb09ae26b5cfa32c0b14862c22bf652f3c8ae
Stefan Dasbach <stefandasbach@gmail.com>
2014-04-14 18:08:35 -0400

    Test joystick button functionality

--- joystickController/LEDController.py
+++ joystickController/LEDController.py
@@ -4,11 +4,38 @@ from time import sleep
 from random import random
 joystick = pygame.joystick
 
+
+def turnOnLED():
+    pass
+
+def changeColor(r, g, b)
+    callToLED('\x80', '\x10', chr(r), chr(g), chr(b), 1)
+
 def main():
     pygame.init()
     print "%d joystick(s) found" % joystick.get_count()
-
+    if joystick.get_count() == 0 : return
+    stick = joystick.Joystick(0)
+    stick.init()
     
+    print "[Joystick] Connected to %s Joystick" % stick.get_name()
+    print "[Joystick] %d axes" % stick.get_numaxes()
+    print "[Joystick] %d balls" % stick.get_numballs()
+    print "[Joystick] %d buttons" % stick.get_numbuttons()
+    print "[Joystick] %d hats" % stick.get_numhats()
+    
+    while True:
+         pygame.event.pump()
+         #Event listeners.
+         if (stick.get_button(2)):
+             # changeColor(0, 0, 0)
+             print "Button 1"
+         if (stick.get_button(3)):
+             print "Button 2"
+         if (stick.get_button(4)):
+             print "Button 3"
+        
+main()
 
 
 """"
